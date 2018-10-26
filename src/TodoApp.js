a48f580c7c452aadd8de2b54e2735901096ab3c4
10109371 <panqi@boe.com.cn>
2018-10-26 11:13:06 +0800

    3.引入bootstrap并编写TodoHeader组件 4.编写代办事项列表组件 5.添加待办事项

---
+++ src/TodoApp.js
@@ -0,0 +1,52 @@
+import React from 'react'
+import 'bootstrap/dist/css/bootstrap.css'
+import TodoHeader from './TodoHeader'
+import TodoItem from './TodoItem'
+import TodoFooter from './TodoFooter'
+
+export default class TodoApp extends React.Component {
+  constructor(props) {
+    super(props) // 父类的构造函数
+    this.state = { // 初始化默认状态
+      todos: [
+        {id: Date.now(), title: '今天学习React', completed: false},
+        {id: Date.now(), title: '今天学习Vue', completed: false}
+      ]
+    }
+  }
+  addTodo = (todo) => {
+    todo = Object.assign({}, {id: Date.now(), completed: false}, todo) // es5语法
+    // todo = {id: Date.now(), completed: false, ...todo} // es7语法，...对象展开符
+    let todos = this.state.todos
+    todos.push(todo)
+    this.setState({todos})
+  }
+  render() {
+    let main = (
+      <ul className="list-group">
+        {
+          this.state.todos.map((todo, index) => <TodoItem todo={todo} key={index}/>)
+        }
+      </ul>
+    )
+    return (
+      <div className="container" style={{marginTop: 20}}>
+        <div className="row">
+          <div className="col-md-6 col-md-offset-3">
+            <div className="panel panel-default">
+              <div className="panel-heading">
+                <TodoHeader addTodo={this.addTodo}/>
+              </div>
+              <div className="panel-body">
+                {main}
+              </div>
+              <div className="panel-footer">
+                <TodoFooter/>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
---
+++ src/TodoHeader.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const ENTRY_KEY = 13
+
+export default class TodoHeader extends React.Component {
+  handleKeyDown = (event) => {
+    if (event.keyCode === ENTRY_KEY) {
+      let title = event.target.value
+      this.props.addTodo({title})
+      event.target.value = ''
+    }
+  }
+
+  render() {
+    return (
+      <div className="form-group">
+        <input type="text" autoFocus={true}
+               onKeyUp={this.handleKeyDown}
+               className="form-control"/>
+      </div>
+    )
+  }
+}
